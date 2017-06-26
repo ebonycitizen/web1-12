115a1145d6bacec0907e991a497eed6be779d80a
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 17:28:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,7 +3,6 @@ var times;
 document.getElementById('form').onsubmit = function()
 {
   window.alert(document.getElementById('form').word.value);
-  IsButtonDown = true;
 }
 
 var times = document.getElementById('form').word.value;
