7c042f197b546746a0d45b11adec2877711fd6fe
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 18:05:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,18 +9,21 @@ document.getElementById('form').onsubmit = function()
 for(var i = 1; i < 100; i++)
 {
   var li = document.createElement('li');
-  if(i%3 === 0)
-  {
-    li.textContent = 'Fizz';
-  }
-  if(i%5 === 0)
-  {
-    li.textContent = 'Buzz';
-  }
   if(i%3 === 0 && i%5 === 0)
   {
     li.textContent = 'FizzBuzz';
   }
-  li.textContent = i;
+  else if(i%3 === 0)
+  {
+    li.textContent = 'Fizz';
+  }
+  else if(i%5 === 0)
+  {
+    li.textContent = 'Buzz';
+  }
+  else
+  {
+    li.textContent = i;
+  }
   document.getElementById('loop').appendChild(li);
 }
