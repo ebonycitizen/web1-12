0cd07f7c419621a14b56eb248469d63879f55ba5
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 18:04:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -13,17 +13,14 @@ for(var i = 1; i < 100; i++)
   {
     li.textContent = 'Fizz';
   }
-  else if(i%5 === 0)
+  if(i%5 === 0)
   {
     li.textContent = 'Buzz';
   }
-  else if(i%3 === 0 && i%5 === 0)
+  if(i%3 === 0 && i%5 === 0)
   {
     li.textContent = 'FizzBuzz';
   }
-  else
-  {
-    li.textContent = i;
-  }
+  li.textContent = i;
   document.getElementById('loop').appendChild(li);
 }
