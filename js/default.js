989cf963df3cc5f39955e0dd594d909251690924
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 17:15:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,7 +3,7 @@ document.getElementById('form').onsubmit = function()
   window.alert(document.getElementById('form').word.value);
 }
 
-for(var i = 1; i < word; i++)
+for(var i = 1; i < word.value; i++)
 {
   var li = document.createElement('li');
   li.textContent = i + '回目';
