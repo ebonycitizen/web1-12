6c802062657240a6f25edaa2fca601c5131331d9
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 17:41:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,35 +3,34 @@ var IsButtonDown = false;
 
 document.getElementById('form').onsubmit = function()
 {
-  window.alert(document.getElementById('form').word.value);
+  document.getElementById('loop').style.visibility="visible";
   IsButtonDown = true;
   return false;
 }
 
 var times = document.getElementById('form').word.value;
+
 if(IsButtonDown)
 {
-  document.getElementById('loop').style.visibility="visible";
-}
-
-for(var i = 1; i < times; i++)
-{
-  var li = document.createElement('li');
-  if(i%3 == 0)
+  for(var i = 1; i < times; i++)
   {
-    li.textContent = 'Fizz';
+    var li = document.createElement('li');
+    if(i%3 == 0)
+    {
+      li.textContent = 'Fizz';
+    }
+    else if(i%5 == 0)
+    {
+      li.textContent = 'Buzz';
+    }
+    else if(i%3 == 0 && i%5 == 0)
+    {
+      li.textContent = 'FizzBuzz';
+    }
+    else
+    {
+      li.textContent = i;
+    }
+    document.getElementById('loop').appendChild(li);
   }
-  else if(i%5 == 0)
-  {
-    li.textContent = 'Buzz';
-  }
-  else if(i%3 == 0 && i%5 == 0)
-  {
-    li.textContent = 'FizzBuzz';
-  }
-  else
-  {
-    li.textContent = i;
-  }
-  document.getElementById('loop').appendChild(li);
 }
