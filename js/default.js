308aa6ce62e73bbd1316fe2ee4bcb9d75372883f
ebonycitizen <coco_coco87@hotmail.com>
2017-06-26 18:01:09 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,27 +1,29 @@
 var times = document.getElementById('form').word.value;
 
-function Show()
+document.getElementById('form').onsubmit = function()
 {
-    for(var i = 1; i < times; i++)
-    {
-      var li = document.createElement('li');
-      if(i%3 === 0)
-      {
-        li.textContent = 'Fizz';
-      }
-      else if(i%5 === 0)
-      {
-        li.textContent = 'Buzz';
-      }
-      else if(i%3 === 0 && i%5 === 0)
-      {
-        li.textContent = 'FizzBuzz';
-      }
-      else
-      {
-        li.textContent = i;
-      }
-      document.getElementById('loop').appendChild(li);
-    }
-    document.getElementById("loop").style.visibility="visible";
+    window.alert(document.getElementById('form').word.value);
+    return false;
+}
+
+for(var i = 1; i < times; i++)
+{
+  var li = document.createElement('li');
+  if(i%3 === 0)
+  {
+    li.textContent = 'Fizz';
+  }
+  else if(i%5 === 0)
+  {
+    li.textContent = 'Buzz';
+  }
+  else if(i%3 === 0 && i%5 === 0)
+  {
+    li.textContent = 'FizzBuzz';
+  }
+  else
+  {
+    li.textContent = i;
+  }
+  document.getElementById('loop').appendChild(li);
 }
